c115f240b8d3aacc3d39482f8f99f1e7e69e15b3
kevin-Alexander23 <no0336032020@unab.edu.sv>
2024-05-28 21:57:53 -0600

    .

---
+++ WindowsFormsApp1/WindowsFormsApp1/Controlador/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Controlador
+{
+    internal class Cliente
+    {
+        public int Clienteid { get; set; }
+        public String Nombre { get; set; }
+        public String Direccion { get; set; }
+        public int Telefono { get; set; }
+        public String Email { get; set; }
+
+
+        public Cliente(int Clienteid, string Nombre, string Direccion, int Telefono, string Email)
+        {
+            this.Clienteid = Clienteid;
+            this.Nombre = Nombre;
+            this.Direccion = Direccion;
+            this.Telefono = Telefono;
+            this.Email = Email;
+
+        }
+    }
+}
